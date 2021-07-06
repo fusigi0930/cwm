426818a779fd61cad64f30bce2c59660b985a64e
ChuYuan Chiang <chuyuan.chiang@gmail.com>
2021-07-06 12:29:34 +0800

    add a test script

---
+++ run-test.lua
@@ -0,0 +1,25 @@
+require "common"
+
+function aml_pre_config()
+
+end
+
+function aml_between_config()
+	local scl = {
+		[0] = 1,
+		[1] = 2,
+	}
+
+	CWM_SettingControl(SCL_CHIP_VENDOR_CONFIG, scl);
+
+	scl = {
+		[0] = 1,
+		[3] = 3,
+		[4] = 5 + 64,
+	}
+	CWM_SettingControl(SCL_LOG, scl);
+end
+
+function aml_poast_config()
+	CWM_SensorEnable(100)
+end
