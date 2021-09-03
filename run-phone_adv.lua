6da83228880cef361f80399d67ee52a998628ab1
ChuYuan Chiang <chuyuan.chiang@gmail.com>
2021-09-03 11:25:09 +0800

    add phone adv script

---
+++ run-phone_adv.lua
@@ -0,0 +1,27 @@
+require "common"
+
+function aml_pre_config()
+	dbgOut("oh ya")
+end
+
+function aml_between_config()
+	local scl = {
+		[0] = 1,
+		[1] = 2,
+	}
+
+	CWM_SettingControl(SCL_CHIP_VENDOR_CONFIG, scl);
+
+	scl = {
+		[0] = 1,
+		[3] = 3,
+		[4] = 5 + 64,
+	}
+	CWM_SettingControl(SCL_LOG, scl);
+	dbgOut("haha")
+end
+
+function aml_post_config()
+	CWM_Sensor_Enable(202)
+	dbgOut("happy")
+end
